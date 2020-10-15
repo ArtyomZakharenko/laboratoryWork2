3d621a3fc65a95f1438ca6123cb7ab179029f8d9
ArtyomZakharenko <61537821+ArtyomZakharenko@users.noreply.github.com>
2020-10-15 13:11:38 +0300

    Update application.c

--- application.c
+++ application.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+#include <conio.h>
 
 void FillArray (int *, int);
 void ShowArray (int *, int);
 int SearchingMaxElement (int *, int);
+void CheckingElementUnderLine (int *, int, int);
 
 int main()
 {
@@ -14,6 +16,7 @@ int main()
     FillArray(array, size);
     ShowArray(array, size);
     maxElement = SearchingMaxElement(array, size);
+    CheckingElementUnderLine(array, size, maxElement);
     
   return 0;
 }
@@ -26,17 +29,20 @@ void FillArray (int *arr, int length){
             printf("Row %d, element %d: ", i+1, j+1);
             scanf("%d", arr+i*length+j);
         }
+        printf("\n");
     }
+    system("cls");
 }   
 void ShowArray(int *arr, int length){
     int i, j;
-    printf("your matrix:\n");
+    printf("your matrix:\n\n");
     for (i = 0; i < length; i++){
         for (j = 0; j < length; j++){
             printf("%4d", *(arr+i*length+j));
         }
         printf("\n");
     }
+    printf("\n");
 }
 
 int SearchingMaxElement (int *arr, int length){
@@ -47,5 +53,18 @@ int SearchingMaxElement (int *arr, int length){
             maxValue = *(arr+i*length+i);
         }
     }
+    printf("The max value of main line is %d\n\n", maxValue);
     return maxValue;
+}
+
+void CheckingElementUnderLine (int * arr, int length, int element){
+    int i, j;
+    for (i = 1; i < length; i++){
+        for(j = 0; j < i; j++){
+            if (*(arr+i*length+j) == element){
+                printf("the same element as main line's max element has coordinates:\n");
+                printf("row: %d, column %d\n\n", i+1, j+1);
+            }
+        }
+    }
 }
